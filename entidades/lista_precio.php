65ca1b3d5cb754979fe077d90097db621284891d
JoshDroid250 <gutierrez25g@gmail.com>
2021-11-07 17:30:36 -0600

    Update_entidades

---
+++ entidades/lista_precio.php
@@ -0,0 +1,16 @@
+<?php
+
+
+class CategoriaP{
+
+private $id_lista_precio;
+private $id_kermesse;
+private $nombre;
+private $descripcion;
+private $estado;
+
+
+
+public function __GET($k){return $this ->$k;}
+public function __SET($k,$v){return $this->$k=$v;}
+}
---
+++ entidades/parroquia.php
@@ -0,0 +1,18 @@
+<?php
+
+
+class parroquia{
+
+private $idParroquia;
+private $nombre;
+private $direccion;
+private $telefono;
+private $parroco;
+private $logo;
+private $sitio_web;
+
+
+
+public function __GET($k){return $this ->$k;}
+public function __SET($k,$v){return $this->$k=$v;}
+}
